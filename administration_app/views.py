f43be466bba228d6e63fd70d06a310f7358aff6a
Michigan_x98😤😤 <bsclmr168023@spu.ac.ke>
2024-03-11 12:55:20 +0300

    Reformatted

--- administration_app/views.py
+++ administration_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
 from django.http import JsonResponse
 from .models import Ballot, RunningMate, Election
+
 
 def check_ballot(request, running_mate_id, election_id):
     user = request.user
@@ -10,7 +10,5 @@ def check_ballot(request, running_mate_id, election_id):
         election = Election.objects.get(id=election_id)
     except (RunningMate.DoesNotExist, Election.DoesNotExist):
         return JsonResponse({'error': 'RunningMate or Election does not exist'}, status=404)
-
     ballot_exists = Ballot.objects.filter(user=user, running_mate=running_mate, election=election).exists()
-
     return JsonResponse({'ballot_exists': ballot_exists})

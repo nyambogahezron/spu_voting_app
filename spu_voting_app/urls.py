85676df49a51a69c79475745c7494c9acd8591ed
Hezron Nyamboga <hezzyn1@gmail.com>
2024-03-11 09:18:02 +0300

    fix static url files

--- spu_voting_app/urls.py
+++ spu_voting_app/urls.py
@@ -10,14 +10,3 @@ urlpatterns = [
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += [
-        re_path(
-            r"^static/images/(?P<path>.*)$",
-            serve,
-            {
-                "document_root": settings.MEDIA_ROOT,
-            },
-        ),
-    ]
